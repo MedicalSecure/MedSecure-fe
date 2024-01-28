4b50949efb9799c9fdbb889d42a13c7792d1db27
AssemToumi <assamtomi@gmail.com>
2024-01-28 23:31:40 +0100

    fix : ng build configuration

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    environmentName: "development",
+    apiUrl: 'https://api.example.com/development',
+    // Add other development-specific configurations here
+  };
