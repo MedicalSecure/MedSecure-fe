9b168e2b9a2c35e36bfd7fa2143b361d25869f78
azaiez hammadi <azaiezhammadi@outlook.com>
2024-02-13 16:36:36 +0100

    start point

---
+++ src/app/add-prescription/add-prescription.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-prescription',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-prescription.component.html',
+  styleUrl: './add-prescription.component.css'
+})
+export class AddPrescriptionComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { AddPrescriptionComponent } from './add-prescription/add-prescription.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'add-prescription', component: AddPrescriptionComponent },
+];
