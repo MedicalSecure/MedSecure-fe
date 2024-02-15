a1e2712f0081b925292a29466ba5f83dc8fb456c
Acil Zaidi <acilazaidi@gmail.com>
2024-02-15 21:10:14 +0100

    form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { RegisterFormComponent } from './register-form/register-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'register', component:RegisterFormComponent },
+];
---
+++ src/app/register-form/register-form.component.ts
@@ -0,0 +1,72 @@
+import { Component } from '@angular/core';
+import {FormBuilder, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MatRadioChange } from '@angular/material/radio';
+import MonitorHeartRoundedIcon from '@mui/icons-material/MonitorHeartRounded';
+interface Activity {
+  value: string;
+  viewValue: string;
+}
+interface Smoking {
+  value: string;
+  viewValue: string;
+}
+@Component({
+  selector: 'app-register-form',
+  standalone: true,
+  imports: [MatButtonModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatSlideToggleModule,
+    MatRadioModule,
+    MatSelectModule,MatCheckboxModule],
+  templateUrl: './register-form.component.html',
+  styleUrl: './register-form.component.css'
+})
+
+export class RegisterFormComponent {
+  selectedSexe: string = ''; 
+
+  handleSexeChange(event: MatRadioChange) {
+    this.selectedSexe = event.value;
+    
+  }
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+  thirdFormGroup = this._formBuilder.group({
+    thirdCtrl: ['', Validators.required],
+  });
+  fourthFormGroup = this._formBuilder.group({
+    fourthCtrl: ['', Validators.required],
+  });
+  isLinear = false;
+  activities: Activity[] = [
+    {value: 'light-0', viewValue: 'light'},
+    {value: 'Medium-1', viewValue: 'Medium'},
+    {value: 'Intense-2', viewValue: 'Intense'},
+  ];
+  habits: Smoking[] = [
+    {value: 'light-0', viewValue: 'light'},
+    {value: 'Medium-1', viewValue: 'Medium'},
+    {value: 'Heavy-2', viewValue: 'Heavy'},
+  ];
+
+  constructor(private _formBuilder: FormBuilder) {}
+}
+
