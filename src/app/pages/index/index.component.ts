f0d41dc9416c7591f6b4a0b0f6cfd7d1b2b3896c
AssemToumi <assamtomi@gmail.com>
2024-02-16 00:37:39 +0100

    fix : admin dashboard template

---
+++ src/app/pages/index/index.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {provideNativeDateAdapter} from '@angular/material/core';
+@Component({
+    selector: 'app-index',
+    standalone: true,
+    templateUrl: './index.component.html',
+    styleUrl: './index.component.css',
+    providers: [provideNativeDateAdapter()],
+    imports: [RouterModule, MatFormFieldModule, MatInputModule, MatDatepickerModule],
+})
+export class IndexComponent {
+
+}
