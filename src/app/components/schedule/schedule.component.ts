eec2bf082c7f8dd4fe897b9301ed954b7610cbab
tissrahma <rahma.tiss@esprit.tn>
2024-02-19 14:05:48 +0100

    feat: fixing schedule

--- src/app/components/schedule/schedule.component.ts
+++ src/app/components/schedule/schedule.component.ts
@@ -10,8 +10,9 @@ import { CommonModule } from '@angular/common';
   styleUrl: './schedule.component.css'
 })
 export class ScheduleComponent implements OnInit {
+   todayDate: string = new Date().toLocaleDateString();
   @Input() selectedHours: any[] = [];
-  @Input() daysOfWeek: string[] = ['31/07', '01/08', '02/08', '03/08', '04/08', '05/08', '06/08'];
+  @Input() daysOfWeek: string[] = [this.todayDate];
   @Input() hoursListEmitter: EventEmitter<any[]> = new EventEmitter<any[]>();
   @Input() keyDownEvent: EventEmitter<any> = new EventEmitter<any>();
   @Input() posologieValue: any;
@@ -26,7 +27,6 @@ export class ScheduleComponent implements OnInit {
   }
   
   onKeyDown(event: any, hourIndex: number, itemIndex: number): void {
-    // You can include hourIndex and itemIndex in the emitted event
     const eventData = { event, hourIndex, itemIndex };
     this.keyDownEvent.emit(eventData);
   }
@@ -44,7 +44,6 @@ if (!isNaN(parsedQuantity)) {
   const incrementedQuantity = parsedQuantity + 1;
   currentitem.quantity = (incrementedQuantity % 1 === 0) ? String(incrementedQuantity) : incrementedQuantity.toFixed(2);
 } else {
-  // If parsing fails, default to 1
   currentitem.quantity = '1';
 }
       }
@@ -61,51 +60,33 @@ if (!isNaN(parsedQuantity)) {
     }
     return '';
   }
-
   hoursList: any[] = [
     [
-      { hour: '06', value: '6', quantity: '1' },
-      { hour: '07', value: '7', quantity: '' },
       { hour: '08', value: '8', quantity: '1' },
-      { hour: '09', value: '9', quantity: '' },
-      { hour: '10', value: '10', quantity: '1' },
-      { hour: '11', value: '11', quantity: '' },
       { hour: '12', value: '12', quantity: '1' },
-      { hour: '13', value: '13', quantity: '' },
-      { hour: '14', value: '14', quantity: '1' },
-      { hour: '15', value: '15', quantity: '' },
-      { hour: '16', value: '16', quantity: '1' },
-      { hour: '17', value: '17', quantity: '' },
+      { hour: '17', value: '17', quantity: '-' },
       { hour: '18', value: '18', quantity: '1' },
-      { hour: '19', value: '19', quantity: '' },
+      { hour: '19', value: '19', quantity: '-' },
       { hour: '20', value: '20', quantity: '1' },
-      { hour: '21', value: '21', quantity: '' },
+      { hour: '21', value: '21', quantity: '-' },
       { hour: '22', value: '22', quantity: '1' },
-      { hour: '23', value: '23', quantity: '' },
+  
       { hour: '00', value: '00', quantity: '1' },
-      { hour: '01', value: '1', quantity: '' },
-      { hour: '02', value: '2', quantity: '1' },
-      { hour: '03', value: '3', quantity: '' },
-      { hour: '04', value: '4', quantity: '1' },
-      { hour: '05', value: '5', quantity: '' },
     ],
   ];
 
   setDesiredHours(): void {
     const desiredHours = [8, 12, 17, 22, 0];
-
     for (const hourArray of this.hoursList) {
       const filteredHours = hourArray.filter((hourObj : any) =>
         desiredHours.includes(parseInt(hourObj.value, 10))
       );
       this.selectedHours.push(filteredHours);
     }
-
     this.selectedHours = this.selectedHours.filter((hourObj, index, self) =>
       index === self.findIndex((h) => h.value === hourObj.value)
     );
   }
-
   toggleCheckbox(currentitem: any) {
     currentitem.isSelected = !currentitem.isSelected;
 }
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -14,35 +14,45 @@ import { DialogueComponent } from '../dialogue/dialogue.component';
 import {MatChipsModule} from '@angular/material/chips';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { ScheduleComponent } from "../components/schedule/schedule.component";
+import {MatTooltipModule} from '@angular/material/tooltip';
+
 
 /**
  * @title Table with pagination
  */
 @Component({
-  selector: 'table-pagination-example',
-  templateUrl: './test.component.html',
-  styleUrl: './test.component.css',
-  standalone: true,
-  imports: [MatTableModule,MatProgressBarModule ,MatGridListModule,MatChipsModule, MatCheckboxModule, MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule , MatPaginatorModule , JsonPipe],
-  animations: [
-    trigger('detailExpand', [
-      state('collapsed', style({ height: '0px', minHeight: '0' })),
-      state('expanded', style({ height: '*' })),
-      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
-    ]),
-  ],
+    selector: 'table-pagination-example',
+    templateUrl: './test.component.html',
+    styleUrl: './test.component.css',
+    standalone: true,
+    animations: [
+        trigger('detailExpand', [
+            state('collapsed', style({ height: '0px', minHeight: '0' })),
+            state('expanded', style({ height: '*' })),
+            transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+        ]),
+    ],
+    imports: [MatTableModule, MatTooltipModule,MatProgressBarModule, MatGridListModule, MatChipsModule, MatCheckboxModule, MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule, MatPaginatorModule, JsonPipe, ScheduleComponent]
 })
 export class TestComponent implements AfterViewInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource<bacpatient>(ELEMENT_DATA);
-  todayDate: string = new Date().toLocaleDateString();
-  columnsToDisplay = ['add', 'room', 'bed', 'patient', 'bd', 'progress','status' ];
-  constructor(public dialog: MatDialog ) { }
-  columnsToDisplayMedicines = ['name', 'posology', 'root']
-  expandedElement: bacpatient | null; 
+  filterByStatus() {
+    const statusOrder: Record<string, number> = { 'Completed': 0, 'On Progress': 1, 'Pending...': 2 };
+    this.dataSource.data = ELEMENT_DATA.sort((a, b) => statusOrder[a.status] - statusOrder[b.status]);
+}
 
   
-  selectedIndex: number | null = null; // Property to track selected index
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = new MatTableDataSource<bacpatient>(ELEMENT_DATA);
+  filterByRoom() {
+    this.dataSource.data= ELEMENT_DATA.sort((a, b) => a.room - b.room);
+   }
+  todayDate: string = new Date().toLocaleDateString();
+  columnsToDisplay = ['add', 'room', 'bed', 'patient', 'age', 'progress','status' ];
+  constructor(public dialog: MatDialog ) { }
+  columnsToDisplayMedicines = ['name', 'posology', 'root']
+  expandedElement: bacpatient | null;
+  selectedIndex: number | null = null; 
   toggleExpanded(element: any) {
     this.expandedElement = this.expandedElement === element ? null : element;
 }
@@ -54,8 +64,6 @@ export class TestComponent implements AfterViewInit {
     }
     this.selectedIndex = index; 
   }
-
- 
   @ViewChild(MatPaginator) paginator: MatPaginator;
  
   ngAfterViewInit() {
@@ -66,18 +74,24 @@ export class TestComponent implements AfterViewInit {
 }
 interface Medicine {
   name: string;
-  posology: string[];
+  posology: Posology[];
   root: string;
   dose : number ; 
   note: string ; 
 }
+export interface Posology {
+  [x: string]: any;
+  hour:string,
+  value:string,
+  quantity:string
+  
+}
 interface bacpatient {
   id: number;
-  us: number;
-  room: string;
-  bed: string;
+  room: number;
+  bed: number;
   patient: string;
-  bd: string;
+    age: number;
   medicines: Medicine[];
   toServe: number;
   served: number;
@@ -86,25 +100,363 @@ interface bacpatient {
   add: string;
 }
 const ELEMENT_DATA: bacpatient[] = [
-  {
-    id: 1,
-    us: 2,
-    room: 'Room1',
-    bed: 'Bed1',
-    patient: 'Patient1',
-    bd: '2/15/2024',
-    medicines: [
-
-      { name: 'medicine17', posology: ["12h", "13h", "14h"], root: 'Injection' , dose:2 ,note:'' },
-      { name: 'medicine18', posology: ["12h", "13h", "14h"], root: 'Oral' ,dose:2, note:''},
-      { name: 'medicine20', posology: ["12h", "13h", "14h"], root: 'Injection' , dose:2 ,note:''}
-    ],
-    toServe: 20,
-    served: 7,
-    status: 'On Progress',
-    note: 'Note 1',
-    add: 'Add 1',
-  },
+    {
+      id: 1,
+      room: 101,
+      bed: 1,
+      patient: 'Patient1',
+      age: 23,
+      medicines: [
+        { name: 'medicine1', posology:  [
+          { hour: '08', value: '8', quantity: '1' },
+          { hour: '12', value: '12', quantity: '1' },
+          { hour: '17', value: '17', quantity: '-' } ,
+          { hour: '22', value: '22', quantity: '1' },
+          { hour: '00', value: '00', quantity: '1' },
+        ], root: 'Injection', dose: 2, note: '' },
+        { name: 'medicine2', posology:  [
+          { hour: '08', value: '8', quantity: '1' },
+          { hour: '12', value: '12', quantity: '1' },
+          { hour: '17', value: '17', quantity: '-' } ,
+          { hour: '22', value: '22', quantity: '1' },
+          { hour: '00', value: '00', quantity: '1' },
+        ], root: 'Oral', dose: 2, note: '' },
+        { name: 'medicine3', posology:  [
+          { hour: '08', value: '8', quantity: '1' },
+          { hour: '12', value: '12', quantity: '1' },
+          { hour: '17', value: '17', quantity: '-' } ,
+          { hour: '22', value: '22', quantity: '1' },
+          { hour: '00', value: '00', quantity: '1' },
+        ], root: 'Injection', dose: 2, note: '' }
+      ],
+      toServe: 20,
+      served: 7,
+      status: 'On Progress',
+      note: 'Note 1',
+      add: 'Add 1'
+    },
+    {
+      id: 2,
+      room: 102,
+      bed: 2,
+      patient: 'Patient2',
+      age: 30,
+      medicines: [
+        { name: 'medicine4', posology:
+        [
+          { hour: '08', value: '8', quantity: '1' },
+          { hour: '12', value: '12', quantity: '1' },
+          { hour: '17', value: '17', quantity: '-' } ,
+          { hour: '22', value: '22', quantity: '1' },
+          { hour: '00', value: '00', quantity: '1' },
+        ]
+        , root: 'Oral', dose: 1, note: '' },
+        { name: 'medicine5', posology: 
+        [
+          { hour: '08', value: '8', quantity: '1' },
+          { hour: '12', value: '12', quantity: '1' },
+          { hour: '17', value: '17', quantity: '-' } ,
+          { hour: '22', value: '22', quantity: '1' },
+          { hour: '00', value: '00', quantity: '1' },
+        ]
+        , root: 'Injection', dose: 1, note: '' },
+        { name: 'medicine6', posology: [
+          { hour: '08', value: '8', quantity: '1' },
+          { hour: '12', value: '12', quantity: '1' },
+          { hour: '17', value: '17', quantity: '-' } ,
+          { hour: '22', value: '22', quantity: '1' },
+          { hour: '00', value: '00', quantity: '1' },
+        ], root: 'Oral', dose: 1, note: '' }
+      ],
+      toServe: 15,
+      served: 5,
+      status: 'Pending...',
+      note: 'Note 2',
+      add: 'Add 2'
+    },
+    {
+      id: 3,
+      room: 101,
+      bed: 3,
+      patient: 'Patient3',
+      age: 50,
+      medicines: [
+        { name: 'medicine7', posology:
+        [
+          { hour: '08', value: '8', quantity: '1' },
+          { hour: '12', value: '12', quantity: '1' },
+          { hour: '17', value: '17', quantity: '-' } ,
+          { hour: '22', value: '22', quantity: '1' },
+          { hour: '00', value: '00', quantity: '1' },
+        ]
+        , root: 'Injection', dose: 3, note: '' },
+        { name: 'medicine8', posology:[
+          { hour: '08', value: '8', quantity: '1' },
+          { hour: '12', value: '12', quantity: '1' },
+          { hour: '17', value: '17', quantity: '-' } ,
+          { hour: '22', value: '22', quantity: '1' },
+          { hour: '00', value: '00', quantity: '1' },
+        ], root: 'Oral', dose: 3, note: '' },
+        { name: 'medicine9',posology:[
+          { hour: '08', value: '8', quantity: '1' },
+          { hour: '12', value: '12', quantity: '1' },
+          { hour: '17', value: '17', quantity: '-' } ,
+          { hour: '22', value: '22', quantity: '1' },
+          { hour: '00', value: '00', quantity: '1' },
+        ]
+        , root: 'Injection', dose: 3, note: '' }
+      ],
+      toServe: 25,
+      served: 10,
+      status: 'Completed',
+      note: 'Note 3',
+      add: 'Add 3'
+    },
+    {
+      id: 4,
+      room: 102,
+      bed: 4,
+      patient: 'Patient4',
+      age: 50,
+      medicines: [
+        { name: 'medicine10', posology:[
+          { hour: '08', value: '8', quantity: '1' },
+          { hour: '12', value: '12', quantity: '1' },
+          { hour: '17', value: '17', quantity: '-' } ,
+          { hour: '22', value: '22', quantity: '1' },
+          { hour: '00', value: '00', quantity: '1' },
+        ], root: 'Oral', dose: 4, note: '' },
+        { name: 'medicine11', posology:[
+          { hour: '08', value: '8', quantity: '1' },
+          { hour: '12', value: '12', quantity: '1' },
+          { hour: '17', value: '17', quantity: '-' } ,
+          { hour: '22', value: '22', quantity: '1' },
+          { hour: '00', value: '00', quantity: '1' },
+        ], root: 'Injection', dose: 4, note: '' },
+        { name: 'medicine12', posology:[
+          { hour: '08', value: '8', quantity: '1' },
+          { hour: '12', value: '12', quantity: '1' },
+          { hour: '17', value: '17', quantity: '-' } ,
+          { hour: '22', value: '22', quantity: '1' },
+          { hour: '00', value: '00', quantity: '1' },
+        ], root: 'Oral', dose: 4, note: '' }
+      ],
+      toServe: 30,
+      served: 20,
+      status: 'On Progress',
+      note: 'Note 4',
+      add: 'Add 4'
+    },
+    {
+      id: 5,
+      room: 101,
+      bed: 5,
+      patient: 'Patient5',
+      age: 35,
+      medicines: [
+        { name: 'medicine13', posology:[
+          { hour: '08', value: '8', quantity: '1' },
+          { hour: '12', value: '12', quantity: '1' },
+          { hour: '17', value: '17', quantity: '-' } ,
+          { hour: '22', value: '22', quantity: '1' },
+          { hour: '00', value: '00', quantity: '1' },
+        ], root: 'Injection', dose: 2, note: '' },
+        { name: 'medicine14', posology:[
+          { hour: '08', value: '8', quantity: '1' },
+          { hour: '12', value: '12', quantity: '1' },
+          { hour: '17', value: '17', quantity: '-' } ,
+          { hour: '22', value: '22', quantity: '1' },
+          { hour: '00', value: '00', quantity: '1' },
+        ], root: 'Oral', dose: 2, note: '' },
+        { name: 'medicine15', posology:[
+          { hour: '08', value: '8', quantity: '1' },
+          { hour: '12', value: '12', quantity: '1' },
+          { hour: '17', value: '17', quantity: '-' } ,
+          { hour: '22', value: '22', quantity: '1' },
+          { hour: '00', value: '00', quantity: '1' },
+        ], root: 'Injection', dose: 2, note: '' }
+      ],
+      toServe: 18,
+      served: 12,
+      status: 'On Progress',
+      note: 'Note 5',
+      add: 'Add 5'
+    },
+    {
+      id: 6,
+      room: 102,
+      bed: 6,
+      patient: 'Patient6',
+      age: 45,
+      medicines: [
+        { name: 'medicine16', posology:[
+          { hour: '08', value: '8', quantity: '1' },
+          { hour: '12', value: '12', quantity: '1' },
+          { hour: '17', value: '17', quantity: '-' } ,
+          { hour: '22', value: '22', quantity: '1' },
+          { hour: '00', value: '00', quantity: '1' },
+        ], root: 'Oral', dose: 3, note: '' },
+        { name: 'medicine17', posology:[
+          { hour: '08', value: '8', quantity: '1' },
+          { hour: '12', value: '12', quantity: '1' },
+          { hour: '17', value: '17', quantity: '-' } ,
+          { hour: '22', value: '22', quantity: '1' },
+          { hour: '00', value: '00', quantity: '1' },
+        ], root: 'Injection', dose: 3, note: '' },
+        { name: 'medicine18', posology:[
+          { hour: '08', value: '8', quantity: '1' },
+          { hour: '12', value: '12', quantity: '1' },
+          { hour: '17', value: '17', quantity: '-' } ,
+          { hour: '22', value: '22', quantity: '1' },
+          { hour: '00', value: '00', quantity: '1' },
+        ], root: 'Oral', dose: 3, note: '' }
+      ],
+      toServe: 22,
+      served: 18,
+      status: 'Completed',
+      note: 'Note 6',
+      add: 'Add 6'
+    },
+    {
+      id: 7,
+      room: 101,
+      bed: 7,
+      patient: 'Patient7',
+      age: 28,
+      medicines: [
+        { name: 'medicine19', posology:[
+          { hour: '08', value: '8', quantity: '1' },
+          { hour: '12', value: '12', quantity: '1' },
+          { hour: '17', value: '17', quantity: '-' } ,
+          { hour: '22', value: '22', quantity: '1' },
+          { hour: '00', value: '00', quantity: '1' },
+        ], root: 'Injection', dose: 1, note: '' },
+        { name: 'medicine20', posology:[
+          { hour: '08', value: '8', quantity: '1' },
+          { hour: '12', value: '12', quantity: '1' },
+          { hour: '17', value: '17', quantity: '-' } ,
+          { hour: '22', value: '22', quantity: '1' },
+          { hour: '00', value: '00', quantity: '1' },
+        ], root: 'Oral', dose: 1, note: '' },
+        { name: 'medicine21', posology:[
+          { hour: '08', value: '8', quantity: '1' },
+          { hour: '12', value: '12', quantity: '1' },
+          { hour: '17', value: '17', quantity: '-' } ,
+          { hour: '22', value: '22', quantity: '1' },
+          { hour: '00', value: '00', quantity: '1' },
+        ], root: 'Injection', dose: 1, note: '' }
+      ],
+      toServe: 12,
+      served: 9,
+      status: 'Pending...',
+      note: 'Note 7',
+      add: 'Add 7'
+    },
+    {
+      id: 8,
+      room: 102,
+      bed: 8,
+      patient: 'Patient8',
+      age: 60,
+      medicines: [
+        { name: 'medicine22', posology:[
+          { hour: '08', value: '8', quantity: '1' },
+          { hour: '12', value: '12', quantity: '1' },
+          { hour: '17', value: '17', quantity: '-' } ,
+          { hour: '22', value: '22', quantity: '1' },
+          { hour: '00', value: '00', quantity: '1' },
+        ], root: 'Oral', dose: 2, note: '' },
+        { name: 'medicine23', posology:[
+          { hour: '08', value: '8', quantity: '1' },
+          { hour: '12', value: '12', quantity: '1' },
+          { hour: '17', value: '17', quantity: '-' } ,
+          { hour: '22', value: '22', quantity: '1' },
+          { hour: '00', value: '00', quantity: '1' },
+        ], root: 'Injection', dose: 2, note: '' },
+        { name: 'medicine24',posology:[
+          { hour: '08', value: '8', quantity: '1' },
+          { hour: '12', value: '12', quantity: '1' },
+          { hour: '17', value: '17', quantity: '-' } ,
+          { hour: '22', value: '22', quantity: '1' },
+          { hour: '00', value: '00', quantity: '1' },
+        ], root: 'Oral', dose: 2, note: '' }
+      ],
+      toServe: 35,
+      served: 25,
+      status: 'On Progress',
+      note: 'Note 8',
+      add: 'Add 8'
+    },
+    {
+      id: 9,
+      room: 101,
+      bed: 0,
+      patient: 'Patient9',
+      age: 32,
+      medicines: [
+        { name: 'medicine25',  posology:[
+          { hour: '08', value: '8', quantity: '1' },
+          { hour: '12', value: '12', quantity: '1' },
+          { hour: '17', value: '17', quantity: '-' } ,
+          { hour: '22', value: '22', quantity: '1' },
+          { hour: '00', value: '00', quantity: '1' },
+        ], root: 'Injection', dose: 1, note: '' },
+        { name: 'medicine26', posology:[
+          { hour: '08', value: '8', quantity: '1' },
+          { hour: '12', value: '12', quantity: '1' },
+          { hour: '17', value: '17', quantity: '-' } ,
+          { hour: '22', value: '22', quantity: '1' },
+          { hour: '00', value: '00', quantity: '1' },
+        ], root: 'Oral', dose: 1, note: '' },
+        { name: 'medicine27', posology:[
+          { hour: '08', value: '8', quantity: '1' },
+          { hour: '12', value: '12', quantity: '1' },
+          { hour: '17', value: '17', quantity: '-' } ,
+          { hour: '22', value: '22', quantity: '1' },
+          { hour: '00', value: '00', quantity: '1' },
+        ], root: 'Injection', dose: 1, note: '' }
+      ],
+      toServe: 28,
+      served: 15,
+      status: 'Completed',
+      note: 'Note 9',
+      add: 'Add 9'
+    },
+    {
+      id: 10,
+      room: 102,
+      bed: 10,
+      patient: 'Patient10',
+      age: 25,
+      medicines: [
+        { name: 'medicine28',posology:[
+          { hour: '08', value: '8', quantity: '1' },
+          { hour: '12', value: '12', quantity: '1' },
+          { hour: '17', value: '17', quantity: '-' } ,
+          { hour: '22', value: '22', quantity: '1' },
+          { hour: '00', value: '00', quantity: '1' },
+        ], root: 'Oral', dose: 3, note: '' },
+        { name: 'medicine29', posology:[
+          { hour: '08', value: '8', quantity: '1' },
+          { hour: '12', value: '12', quantity: '1' },
+          { hour: '17', value: '17', quantity: '-' } ,
+          { hour: '22', value: '22', quantity: '1' },
+          { hour: '00', value: '00', quantity: '1' },
+        ], root: 'Injection', dose: 3, note: '' },
+        { name: 'medicine30', posology:[
+          { hour: '08', value: '8', quantity: '1' },
+          { hour: '12', value: '12', quantity: '1' },
+          { hour: '17', value: '17', quantity: '-' } ,
+          { hour: '22', value: '22', quantity: '1' },
+          { hour: '00', value: '00', quantity: '1' },
+        ], root: 'Oral', dose: 3, note: '' }
+      ],
+      toServe: 16,
+      served: 8,
+      status: 'Pending...',
+      note: 'Note 10',
+      add: 'Add 10'
+    }
+  ] ;
   
-
-];
+  
