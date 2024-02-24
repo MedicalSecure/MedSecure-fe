9be0e73b4db33e7f2592c67c564bf397c3dd533e
azaiez hammadi <azaiezhammadi@outlook.com>
2024-02-24 14:47:11 +0100

    feat: add column mapping

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,12 @@ import { MedicationSearchComponent } from './medication-search/medication-search
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { Page1Component } from './pages/page1/page1.component';
 import { MedicinesTableComponent } from './medicines-table/medicines-table.component';
+import { PharmacyComponent } from './pages/pharmacy/pharmacy.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-    { path: 'dashboard', component: DashboardComponent },
-    { path: 'page1', component: Page1Component } ,
-    {path: 'search', component: MedicationSearchComponent},
-    {path: 'pharmacy', component: MedicinesTableComponent},
-
-    
-
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'page1', component: Page1Component },
+  { path: 'search', component: MedicationSearchComponent },
+  { path: 'pharmacy', component: PharmacyComponent },
 ];
