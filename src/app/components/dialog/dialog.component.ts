42eeddb84b9996a740e3743da8058d2b9520ca5a
AssemToumi <assamtomi@gmail.com>
2024-02-27 10:41:39 +0100

    feat : masonry

--- src/app/components/dialog/dialog.component.ts
+++ src/app/components/dialog/dialog.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { ScheduleComponent } from "../schedule/schedule.component";
+import { MasonryComponent } from "../masonry/masonry.component";
 
 @Component({
     selector: 'app-dialog',
     standalone: true,
     templateUrl: './dialog.component.html',
     styleUrl: './dialog.component.css',
-    imports: [ScheduleComponent]
+    imports: [ScheduleComponent, MasonryComponent]
 })
 export class DialogComponent {
 
