62f362d3be3b38089e1f09b83fa8a865532f3caf
AssemToumi <assamtomi@gmail.com>
2024-02-27 22:31:48 +0100

    feat : date picker

--- src/app/components/datepicker-range-popup/datepicker-range-popup.component.ts
+++ src/app/components/datepicker-range-popup/datepicker-range-popup.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { CalendarModule } from 'primeng/calendar';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-datepicker-range-popup',
   standalone: true,
-  imports: [],
+  imports: [CalendarModule, FormsModule],
   templateUrl: './datepicker-range-popup.component.html',
   styleUrl: './datepicker-range-popup.component.css'
 })
 export class DatepickerRangePopupComponent {
-
+  date: Date = new Date();
 }
