b9e7dce1fb7134b61d2cc753b6d47f08a784d6e4
AssemToumi <63657019+AssemToumi@users.noreply.github.com>
2024-02-27 14:25:36 +0100

    feat : parts of day (#12)

--- src/app/components/dialog/dialog.component.ts
+++ src/app/components/dialog/dialog.component.ts
@@ -2,17 +2,13 @@ import { Component } from '@angular/core';
 import { ScheduleComponent } from "../schedule/schedule.component";
 import { MasonryComponent } from "../masonry/masonry.component";
 import { PartsOfDayComponent } from "../parts-of-day/parts-of-day.component";
-import { TimelineComponent } from "../timeline/timeline.component";
-import { ShipsSelectComponent } from "../chips-select/chips-select.component";
-import { MedicationSearchComponent } from "../medication-search/medication-search.component";
-import { WizardHeaderComponent } from "../wizard-header/wizard-header.component";
 
 @Component({
     selector: 'app-dialog',
     standalone: true,
     templateUrl: './dialog.component.html',
     styleUrl: './dialog.component.css',
-    imports: [ScheduleComponent, MasonryComponent, PartsOfDayComponent, TimelineComponent, WizardHeaderComponent, MedicationSearchComponent, ShipsSelectComponent]
+    imports: [ScheduleComponent, MasonryComponent, PartsOfDayComponent]
 })
 export class DialogComponent {
 
