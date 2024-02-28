701de9b8eef561b50a1a8da52de2df5afe0eba07
AssemToumi <63657019+AssemToumi@users.noreply.github.com>
2024-02-28 02:31:50 +0100

    Feat/datepicker (#13)
    * feat : datepicker
    
    * feat : parts of day
    
    * feat : date picker
    
    * feat : Masonry
    
    * feat : Date range picker
    
    * feat : timeline

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,10 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideDaterangepickerLocale } from "ngx-daterangepicker-bootstrap";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideRouter(routes), 
+              provideDaterangepickerLocale({separator: ' - ', applyLabel: 'Okay',}), 
+              provideAnimationsAsync()]
 };
