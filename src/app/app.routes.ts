e610342fa4983db04a08bf1b331ad0ae797396f2
Acil Zaidi <acilazaidi@gmail.com>
2024-03-04 15:06:11 +0100

    masonry-layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { RegisterFormComponent } from './register-form/register-form.component';
 import { RegisterViewComponent } from './register-view/register-view.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { Page1Component } from './pages/page1/page1.component';
+import { MasonryDpiComponent } from './components/masonry-dpi/masonry-dpi.component';
+
 
 export const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'register', component: RegisterFormComponent },
-  
+  { path: 'view', component: MasonryDpiComponent },
   { path: 'register-view', component: RegisterViewComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'page1', component: Page1Component },
