ad5c0a5b91b791c75d5d21c7b0d0e8668c7ba264
jamelChadha <126973644+jamelChadha@users.noreply.github.com>
2024-03-15 13:33:52 +0100

    feat : add scheduler component (#17)
    * initial commit without detail
    
    * feat: update form fron json file
    
    * fix: fixing conflicts
    
    * feat: decompose into components
    
    * feat update add sheduler component
    
    * feat: correct component-appointement en utilisant des classes Bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { IndexComponent } from "./pages/index/index.component";
+import { CalendarEvent, CalendarEventAction } from 'angular-calendar';
+import { CalendarShedulerComponent  } from './components/calendar-scheduler/calendar-scheduler.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
+    schemas: [NO_ERRORS_SCHEMA],
     imports: [RouterOutlet, IndexComponent]
 })
 export class AppComponent {
+
+  constructor() { }
   title = 'medsecure-fe';
+
+
 }
