bf5fe16f9b49a93820541671302cb8e3a2b2bc8a
aymen-elhajji <102991501+aymen-elhajji@users.noreply.github.com>
2024-03-22 00:37:48 +0100

    fix: fix table style and fix dialog style and add hint label

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { Page1Component } from './pages/page1/page1.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { PharmacyComponent } from './pages/pharmacy/pharmacy.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
     { path: 'dashboard', component: DashboardComponent },
     { path: 'page1', component: Page1Component } ,
     { path: 'search', component: SearchBarComponent},
+    { path: 'pharmacy', component: PharmacyComponent }
 ];
