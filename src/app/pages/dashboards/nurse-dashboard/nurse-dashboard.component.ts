bed81c128d7423ccf1c342387bf88312e31438ab
tissrahma <rahma.tiss@esprit.tn>
2024-05-20 17:01:05 +0200

    fix : dashboard-updates

--- src/app/pages/dashboards/nurse-dashboard/nurse-dashboard.component.ts
+++ src/app/pages/dashboards/nurse-dashboard/nurse-dashboard.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivitiesComponent } from '../../../components/activities/activities.component';
 import { BacPatientService } from '../../../services/bacPatient/bac-patient-services.service';
+import { ELEMENT_DATA, bacpatient } from '../../bacPatient/bacPatient.component';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-nurse-dashboard',
@@ -9,6 +11,33 @@ import { BacPatientService } from '../../../services/bacPatient/bac-patient-serv
   templateUrl: './nurse-dashboard.component.html',
   styleUrl: './nurse-dashboard.component.css'
 })
-export class NurseDashboardComponent {
+export class NurseDashboardComponent implements OnInit {
+patientNumber: number = 100;
+checkedIn: number = 80;
+checkedOut: number = 20;
+pending: number = 0;
+onProgress: number = 0;
+completed: number = 0;
+data : bacpatient[] = [] ;
+dataSource = new MatTableDataSource(ELEMENT_DATA);
 constructor(public bacPatientService : BacPatientService){}
+  ngOnInit(): void {
+  this.data = this.bacPatientService.getData(this.dataSource);
+  this.calculateStatus();
+  }
+calculateStatus(){
+ this.data.forEach(bacPatient=>{
+    if(bacPatient.status===0){
+      this.pending++;
+    }else if(bacPatient.status===1){
+      this.onProgress++;
+    }if(bacPatient.status===2){
+      this.completed++;
+    }
+  
+  }
+
+  )
+}
+
 }
