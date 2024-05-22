c84233b6777d3862cdd768353d625a60b0e2590f
tissrahma <rahma.tiss@esprit.tn>
2024-05-22 13:47:15 +0200

    diets_screen_first_commit

---
+++ src/app/pages/diet/home/diet.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { WizardHeaderComponent } from "../../../components/wizard-header/wizard-header.component";
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AddDietComponent } from "../steps/add-diet/add-diet.component";
+@Component({
+    selector: 'app-diet',
+    standalone: true,
+    templateUrl: './diet.component.html',
+    styleUrl: './diet.component.css',
+    imports: [WizardHeaderComponent , CommonModule ,RouterModule , AddDietComponent]
+    
+})
+
+export class DietComponent {
+  stepNumber: number = 1;
+  steps: wizardStepType[] = [
+    {
+      id: 1,
+      title: 'Patient Selection',
+      matIconName: '',
+      iconClass: 'fa fa-list',
+    },
+    {
+      id: 2,
+      title: 'Patient Details',
+      matIconName: '',
+      iconClass: 'fa fa-user-md',
+    },
+    {
+      id: 3,
+      title: 'Add Diet',
+      matIconName: '',
+      iconClass: 'fa fa-apple-alt',
+    },
+    {
+      id: 4,
+      title: 'Add Meal',
+      matIconName: '',
+      iconClass: 'fa fa-utensils', 
+    }    
+  ];
+    SwitchToStep(number: number) {
+    this.stepNumber = number;
+  }
+}
+export type wizardStepType = {
+  id: number;
+  title: string;
+  matIconName: string;
+  iconClass: string;
+};
+
