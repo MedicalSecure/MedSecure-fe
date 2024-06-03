552436d7d4ff18454ea46054f584d2bb8a9d9278
tissrahma <rahma.tiss@esprit.tn>
2024-06-03 22:35:46 +0200

    prescription list

--- src/app/pages/diet/steps/prescription-list/prescription-list.component.ts
+++ src/app/pages/diet/steps/prescription-list/prescription-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
   EventEmitter,
@@ -7,132 +7,187 @@ import {
   SimpleChanges,
 } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { PrescriptionStatus, HistoryStatus } from '../../../../enums/enum';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { SnackBarMessagesComponent, snackbarMessageType } from '../../../../components/snack-bar-messages/snack-bar-messages.component';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatIcon } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 import { FilterPatientByNameAndSnPipe } from '../../../../pipes/filter-patient-by-name-and-id/filter-patient-by-name-and-sn.pipe';
 import { AddDietComponent } from "../add-diet/add-diet.component";
 import { PatientDetailComponent } from "../patient-detail/patient-detail.component";
+import { PrescriptionApiService } from '../../../../services/prescription/prescription-api.service';
+import { PrescriptionDto, RegisterForPrescription, RegisterWithPrescriptions } from '../../../../model/Prescription';
+import { RegisterDto } from '../../../../model/Registration';
+import { calculateAge, getDateString, getTimeString } from '../../../../shared/utilityFunctions';
 
+import { mapRegisterWithPrsToRegisterForPrs } from '../../../../shared/DTOsExtensions';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatDatepicker ,MatDatepickerModule} from '@angular/material/datepicker';
+import { MatSort } from '@angular/material/sort';
+import { MatChip } from '@angular/material/chips';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 @Component({
-    selector: 'app-prescription-list',
+    selector: 'app-prescription-list-for-diet',
     standalone: true,
     templateUrl: './prescription-list.component.html',
     styleUrl: './prescription-list.component.css',
     imports: [
+      MatDatepicker ,MatDatepickerModule,
+      MatFormFieldModule,
+      MatInputModule,
         CommonModule,
         FormsModule,
         FilterPatientByNameAndSnPipe,
         MatIcon,
         RouterModule,
         AddDietComponent,
-        PatientDetailComponent
+        PatientDetailComponent,
+        RouterModule,
+        CommonModule,
+        PrescriptionListComponent,
+        SnackBarMessagesComponent,
+        MatProgressSpinnerModule,
+        CommonModule,
+        FormsModule,
+        MatIcon,
+        RouterModule
+        , MatTableModule,
+        MatTabsModule,
+        MatChip
     ]
 })
-export class PrescriptionListComponent {
-  @Input() selectedPrescription: any | undefined = undefined;
-  @Output() onClickNewPrescriptionEvent = new EventEmitter<boolean>();
-  @Input() clearTextAfterEachSearch: boolean = false;
-  @Input()
-  dataList: any[] = [
-    {
-      sn: '001',
-      name: 'John',
-      sex: 'Male',
-      age: 30,
-      height: 180,
-      registrationDate: new Date(),
-    },
-    {
-      sn: '002',
-      name: 'Jane',
-      sex: 'Female',
-      age: 25,
-      height: 165,
-      registrationDate: new Date(),
-    },
-    {
-      sn: '003',
-      name: 'Alice',
-      sex: 'Female',
-      age: 28,
-      height: 170,
-      registrationDate: new Date(),
-    },
-    {
-      sn: '004',
-      name: 'Bob',
-      sex: 'Male',
-      age: 35,
-      height: 175,
-      registrationDate: new Date(),
-    },
-    {
-      sn: '005',
-      name: 'Eve',
-      sex: 'Female',
-      age: 22,
-      height: 160,
-      registrationDate: new Date(),
-    },
-    {
-      sn: '006',
-      name: 'Mike',
-      sex: 'Male',
-      age: 32,
-      height: 185,
-      registrationDate: new Date(),
-    },
-    {
-      sn: '007',
-      name: 'Sarah',
-      sex: 'Female',
-      age: 27,
-      height: 168,
-      registrationDate: new Date(),
-    },
-    {
-      sn: '008',
-      name: 'David',
-      sex: 'Male',
-      age: 29,
-      height: 176,
-      registrationDate: new Date(),
-    },
-    {
-      sn: '009',
-      name: 'Emily',
-      sex: 'Female',
-      age: 31,
-      height: 162,
-      registrationDate: new Date(),
-    },
-    {
-      sn: '010',
-      name: 'Alex',
-      sex: 'Male',
-      age: 26,
-      height: 178,
-      registrationDate: new Date(),
-    },
-  ];
-  checked: boolean = true;
-  searchTerm: string = '';
-
-  onClickPrescription(Prescription: any) {
-    this.selectedPrescription = Prescription;
-  }
-
-  ngOnChanges(changes: SimpleChanges) {
-    if (!this.clearTextAfterEachSearch) return;
-    let newChange = changes['selectedPatient'];
-    if (newChange && !newChange.firstChange) {
-      if (this.selectedPrescription === undefined) this.searchTerm = '';
+export class PrescriptionListComponent implements OnChanges {
+    @Input() selectedRegister: RegisterForPrescription | undefined = undefined;
+    @Output() selectedRegisterChange = new EventEmitter<
+      RegisterForPrescription | undefined
+    >();
+    @Output() onIsPatientSelectPageValidChange = new EventEmitter<boolean>();
+    @Input() clearTextAfterEachSearch: boolean = false;
+    @Input()
+    RegistrationsList: RegisterForPrescription[] = [];
+    IsPatientListLoading = false;
+    isFailedToLoad = false;
+    selectedDate: Date = new Date();
+    dataSource = new MatTableDataSource([] as RegisterForPrescription[]);
+    ErrorMessage = '';
+  
+    columnsToDisplay: (keyof RegisterForPrescription)[] = [
+      'mrn',
+      'patient_fullName', //name
+      'patient_dateOfBirth', //age
+      'registeredAt',
+      'patient_id', //look
+    ];
+  
+    checked: boolean = true;
+    searchTerm: string = '';
+  
+    @ViewChild(MatSort) sort: MatSort;
+    @ViewChild('picker') picker: MatDatepicker<Date>;
+  
+    constructor(private prescriptionApiService:PrescriptionApiService) {}
+  
+    ngOnInit() {
+      this.fetchRegistrationsWithPrescriptions();
+    }
+  
+    ngOnChanges(changes: SimpleChanges) {
+      if (!this.clearTextAfterEachSearch) return;
+      let newChange = changes['selectedRegister'];
+      if (newChange && !newChange.firstChange) {
+        if (this.selectedRegister === undefined) this.searchTerm = '';
+      }
+    }
+  
+    onClickPatient(selectedRegister: RegisterForPrescription) {
+      this.selectedRegisterChange.emit(selectedRegister);
+      this.selectedRegister = selectedRegister;
+      this.onIsPatientSelectPageValidChange.emit(selectedRegister != undefined);
+      console.log("hello");
+    }
+    async fetchRegistrationsWithPrescriptions() {
+      try {
+        this.isFailedToLoad = false;
+        this.IsPatientListLoading = true;
+        var dictByRegisterIdResponse =
+          await PrescriptionApiService.getRegistrationsWithPrescriptions(
+            this.prescriptionApiService
+          );
+        if(!dictByRegisterIdResponse) throw Error("Cant fetch registers with prescriptions");
+        let registerWithPrescriptionsList = Object.values(dictByRegisterIdResponse);
+        this.RegistrationsList=registerWithPrescriptionsList.map(item=>mapRegisterWithPrsToRegisterForPrs(item))
+        
+        //refresh the filter
+        this.changeDate((new Date()).toDateString())
+        
+        this.IsPatientListLoading = false;
+        this.isFailedToLoad = false;
+        this.ErrorMessage = '';
+      } catch (error) {
+        this.ErrorMessage = "Can't fetch registrations";
+        console.error(error);
+        this.isFailedToLoad = true;
+      }
+    }
+  
+    changeDate(selectedDate: string) {
+      this.selectedDate = new Date(selectedDate);
+      let newSelectedDate = this.selectedDate.getDate();
+      this.dataSource.data = this.RegistrationsList.filter(
+        (item) =>
+          new Date(item.createdAt ?? new Date()).getDate() === newSelectedDate
+      );
+    }
+  
+    onLeftButtonClick() {
+      this.selectedDate.setDate(this.selectedDate.getDate() - 1);
+      let currentSelectedDate = this.selectedDate.getDate();
+      this.dataSource.data = this.RegistrationsList.filter(
+        (item) =>
+          new Date(item.createdAt ?? new Date()).getDate() === currentSelectedDate
+      );
+    }
+  
+    onRightButtonClick() {
+      this.selectedDate.setDate(this.selectedDate.getDate() + 1);
+      let currentSelectedDate = this.selectedDate.getDate();
+      this.dataSource.data = this.RegistrationsList.filter(
+        (item) =>
+          new Date(item.createdAt ?? new Date()).getDate() === currentSelectedDate
+      );
+    }
+  
+    Filter(event: Event) {
+      const filterValue = (event.target as HTMLInputElement).value;
+      this.dataSource.filterPredicate = (data: RegisterForPrescription, filter: string) => {
+        const p1 = data.patient_firstName.toString().toLowerCase();
+        const p2 = data.patient_lastName.toString().toLowerCase();
+        const byMRN = data.id?.toString().toLowerCase().includes(filter);
+        if(byMRN) return true;
+  
+        const byName1 = (p1 + " " + p2).includes(filter)
+        if(byName1) return true;
+  
+        const byName2 = (p2 + " " + p1).includes(filter)
+        if(byName2) return true;
+  
+        return false;
+      };
+      this.dataSource.filter = filterValue.trim().toLowerCase();
+    }
+  
+    calculateAge(dateOfBirth: Date): number {
+      const today = new Date();
+      const birthDate = new Date(dateOfBirth);
+      let age = today.getFullYear() - birthDate.getFullYear();
+      const month = today.getMonth() - birthDate.getMonth();
+      if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+      }
+      return age;
     }
   }
-
-  onClickNewPrescription() {
-    this.onClickNewPrescriptionEvent.emit(false);
-    this.onClickPrescription(undefined);
-  }
-}
+  
+ 
