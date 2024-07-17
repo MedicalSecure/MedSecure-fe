8a5a54b7a914b4c3f817690374a096734a6a151b
HamadiAZ <azaiezhammadii@gmail.com>
2024-07-17 17:51:27 +0100

    fix: fix diet page display by role

--- src/app/pages/diet/home/diet.component.ts
+++ src/app/pages/diet/home/diet.component.ts
@@ -1,23 +1,34 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { WizardHeaderComponent } from "../../../components/wizard-header/wizard-header.component";
+import { WizardHeaderComponent } from '../../../components/wizard-header/wizard-header.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { AddDietComponent } from "../steps/add-diet/add-diet.component";
-import { PrescriptionListComponent } from "../steps/prescription-list/prescription-list.component"
-import { PatientDetailComponent } from "../steps/patient-detail/patient-detail.component"
+import { AddDietComponent } from '../steps/add-diet/add-diet.component';
+import { PrescriptionListComponent } from '../steps/prescription-list/prescription-list.component';
+import { PatientDetailComponent } from '../steps/patient-detail/patient-detail.component';
 import { RegisterForPrescription } from '../../../model/Prescription';
 import foods, { Diet, Food, Meal } from '../../../model/Diet';
 import { MealsListComponent } from '../steps/meals-list/meals-list.component';
-import { RoleAuthGuard } from '../../../../app/role-auth.guard'
+import { getRoles, RoleAuthGuard } from '../../../../app/role-auth.guard';
+import { AzureGraphService } from '../../../azure-graph.service';
+import { User } from '../../account/account.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-diet',
   standalone: true,
   templateUrl: './diet.component.html',
   styleUrl: './diet.component.css',
-  imports: [WizardHeaderComponent, MealsListComponent, CommonModule, RouterModule, AddDietComponent, PatientDetailComponent, PrescriptionListComponent]
+  imports: [
+    WizardHeaderComponent,
+    MealsListComponent,
+    CommonModule,
+    RouterModule,
+    AddDietComponent,
+    PatientDetailComponent,
+    PrescriptionListComponent,
+    MatProgressSpinnerModule
+  ],
 })
-
 export class DietComponent implements OnInit {
   stepNumber: number = 1;
   updatingOldPrescriptionMode = false;
@@ -36,13 +47,55 @@ export class DietComponent implements OnInit {
   currentDiet: Diet;
   @Output() DietEmit = new EventEmitter<Diet>();
 
-  url: string;
+  isPageLoading=true;
+  currentUser: User | undefined;
+
   @Output() emitCanValidate = new EventEmitter<boolean>();
   dietToEdit: Diet;
-  constructor(public roleAuth: RoleAuthGuard) { }
+  constructor(private graphService: AzureGraphService) {}
   ngOnInit(): void {
-    this.url = this.roleAuth.profile?.jobTitle as string;
+    this.isPageLoading=true
+    this.graphService.getCurrentUserWithRole().subscribe((account) => {
+      console.log(account);
+      
+      this.currentUser = account;
+      this.isPageLoading=false;
+    });
   }
+
+  getRoles() {
+    // to be used with HTML
+    //debugger;
+    return getRoles(this.currentUser);
+  }
+
+
+
+  SwitchToStep(number: number) {
+    this.stepNumber = number;
+    // Disallow going back once navigated forward
+  }
+  onDietEmmitted(diet: any) {
+    this.currentDiet = diet;
+    this.DietEmit.emit(this.currentDiet);
+    console.log('Diet emitted:', diet);
+  }
+  onSelectPatientChange(register: RegisterForPrescription | undefined) {
+    this.stepNumber = 2;
+    this.inputRegister = register;
+    this.selectedRegister = register;
+  }
+  onMealsEmitter(meals: Meal[]) {
+    this.emittedMeals = meals;
+    this.emittedShowMeal = true;
+    this.canNavigate = true;
+    this.emitCanValidate.emit(this.canNavigate);
+  }
+  onFoodEmmited(food: Food) {
+    this.emmitedFoods = food;
+    this.emittedShowFood = true;
+  }
+
   steps: wizardStepType[] = [
     {
       id: 1,
@@ -63,40 +116,6 @@ export class DietComponent implements OnInit {
       iconClass: 'fa fa-hamburger',
     },
   ];
-  SwitchToStep(number: number) {
-
-    this.stepNumber = number;
-    // Disallow going back once navigated forward
-
-  }
-  onDietEmmitted(diet: any) {
-    this.currentDiet = diet;
-    this.DietEmit.emit(this.currentDiet)
-    console.log('Diet emitted:', diet);
-  }
-  onSelectPatientChange(register: RegisterForPrescription | undefined) {
-    this.stepNumber = 2;
-    this.inputRegister = register;
-    this.selectedRegister = register;
-
-
-  }
-  onMealsEmitter(meals: Meal[]) {
-    this.emittedMeals = meals;
-    this.emittedShowMeal = true;
-    this.canNavigate = true
-    this.emitCanValidate.emit(this.canNavigate);
-
-
-  }
-  onFoodEmmited(food: Food) {
-
-    this.emmitedFoods = food;
-    this.emittedShowFood = true;
-  }
-
-
-
 }
 const _nextButtonContent = {
   label: 'next',
@@ -113,4 +132,3 @@ export type wizardStepType = {
   matIconName: string;
   iconClass: string;
 };
-
